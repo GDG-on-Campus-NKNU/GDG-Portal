8e1ecff0e984f12478c050769db05ecc610a8eb4
Bernie Yen <ptyc4076@gmail.com>
2025-03-22 00:49:02 +0800

    Removed deprecation warning, updated project structure, added a .editorconfig. 03/22

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,3 @@
-﻿// client/src/App.js
-import { useEffect, useState } from 'react'
-import './App.css'
-function App() {
-    const [message, setMessage] = useState('')
-
-    useEffect(() => {
-        fetch('http://localhost:5000/api/hello')
-            .then(res => res.json())
-            .then(data => setMessage(data.message))
-    }, [])
-
-    return (
-        <div className="App">
-            <h1>GDG Frontend</h1>
-            <p>後端回傳：{message}</p>
-        </div>
-    )
-}
+﻿import App from './components/App'
 
 export default App
