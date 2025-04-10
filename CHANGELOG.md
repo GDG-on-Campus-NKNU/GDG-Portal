fc04af3c3aa3c75f0cdc94cf46fe58974e9e6334
Bernie <ptyc4076@gmail.com>
2025-04-11 01:40:24 +0800

    Integrated .env. 04/11

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## Integrated .env. 04/11
+> 2025-04-11 01:40:24
+> Author: Bernie <ptyc4076@gmail.com>
+> Commit: `1c394224268de649ecd21965d88491ebeda4375d`
+
+---
+
--- server/app.js
+++ server/app.js
@@ -10,10 +10,14 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.send('伺服器運行中 🚀');
+});
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from GDG Backend! We\'ll go from here now.' })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`✅ Server running on http://localhost:${PORT}`)
 })
